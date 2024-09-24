aee03b231df71113e9df19c58323db452e132a08
samhitareddy <samhitareddy0402@gmail.com>
2024-09-23 23:35:41 -0400

    task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numToExamine = 4;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println (numToExamine + " is Even.");
+        } else {
+            System.out.println (numToExamine + " is Odd.");
+        }
     }
 }
